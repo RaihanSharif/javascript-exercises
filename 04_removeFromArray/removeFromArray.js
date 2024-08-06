7b9ca0510762d3bdef182c9218afdef97fb81c16
Raihan Sharif <raihansharif92@gmail.com>
2024-08-06 17:11:29 +0100

    exercise 4 all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
+    const args = Array.from(arguments).slice(1);
+    let tempArr = [];
+    arguments[0].forEach(element => {
+        if (!(args.includes(element))) {
+            tempArr.push(element);
+        }
+    });
+    return tempArr;
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
