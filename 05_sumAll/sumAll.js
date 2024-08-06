bf5c7d523da271ccade1e0b7f64274bbb450ed70
Raihan Sharif <raihansharif92@gmail.com>
2024-08-06 18:18:37 +0100

    Ex 5: can sum from arbitrary low number to high number, not just from 1.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    
+    const args = Array.from(arguments);
 
+    for (elem of args) {
+        if (elem <= 0 || !(Number.isInteger(elem)) || typeof elem != "number") {
+            return 'ERROR';
+        }
+    }
+
+    const big = Math.max(first, second);
+    const small = Math.min(first, second);
+
+
+
+    return (big * (big + 1)/2) - ((small-1) * (small)/2);
 };
 
 // Do not edit below this line
