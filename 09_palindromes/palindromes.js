7da30c6ea2c2620bb12aee5c40d4850af09a24b8
Raihan Sharif <raihansharif92@gmail.com>
2024-08-11 15:13:24 +0100

    exercise 9 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    // strip non-alphanumeric chars, ignore case
+    input = input.replace(/[^a-zA-Z0-9]/g, '').toLowerCase(); 
+    const reversed = input.split("").reverse().join("");
+    return input == reversed;
 };
 
 // Do not edit below this line
